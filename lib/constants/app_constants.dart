4eda7e4b018865e8b31a536c83b703bbb09bfef0
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-22 23:16:28 +0900

    add constants for google signin scopes

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -2,4 +2,6 @@ class AppConstants {
   static const privacyPolicyEN = "https://docs.google.com/document/d/1jqJidv7-r0o_1E5JJrP3ebDIbMw05jM_Wl300hIWBlk/edit?usp=sharing";
   static const privacyPolicyKO = "https://docs.google.com/document/d/1gIpFSEZ6hHXe7lBepQXcO7hp0X7pZNRorcLO8Y3l1Ow/edit?usp=sharing";
   static const openAIUsagePolicy = "https://openai.com/policies/usage-policies";
+  static const googleDriveSignInScope = "Google Drive Sign In";
+  static const googleNormalSignInScope = "Google Normal Sign In";
 }
