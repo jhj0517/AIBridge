2b1d8062d60862e7c7abb6872a2ce61c46e07688
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-22 23:16:14 +0900

    add modules

---
+++ lib/di/modules/repository_module.dart
@@ -0,0 +1,19 @@
+import 'package:aibridge/localdb/localdb.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../repositories/repositories.dart';
+import '../dependency_injection.dart';
+import 'base_module.dart';
+
+final class RepositoryModule extends BaseModule {
+
+  @override
+  Future<void> register() async {
+    locator.registerFactory(() => CharactersRepository(sqfliteHelper: locator.get<SQFliteHelper>()));
+    locator.registerFactory(() => ChatRepository(prefs: locator.get<SharedPreferences>(), sqfliteHelper: locator.get<SQFliteHelper>()));
+    locator.registerFactory(() => ChatRoomRepository(prefs: locator.get<SharedPreferences>(), sqfliteHelper: locator.get<SQFliteHelper>()));
+    locator.registerFactory(() => KeyRepository(secureStorage: locator.get<FlutterSecureStorage>()));
+  }
+
+}
