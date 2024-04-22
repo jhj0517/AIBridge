a3aaca8ff5af72208926df6934da3bf209b91a5d
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-22 23:16:35 +0900

    apply di

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'firebase_options.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'generated/l10n.dart';
 import 'pages/pages.dart';
@@ -17,42 +13,23 @@ import 'constants/constants.dart';
 import 'providers/providers.dart';
 import 'repositories/repositories.dart';
 import 'localdb/localdb.dart';
-
-Future<SharedPreferences> _initSharedPreference() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  return await SharedPreferences.getInstance();
-}
-
-Future<void> _initFirebase() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-}
-
-Future<void> _initDotEnv() async{
-  await dotenv.load(fileName: ".env");
-}
+import 'di/dependency_injection.dart';
 
 void main() async {
-  await _initDotEnv();
-  final SharedPreferences prefs = await _initSharedPreference();
-  await _initFirebase();
+  WidgetsFlutterBinding.ensureInitialized();
+  await DependencyInjection.register();
 
   runApp(
       Phoenix(
-          child: MyApp(
-            prefs: prefs,
-          )
+          child: const MyApp()
       )
   );
 }
 
 class MyApp extends StatelessWidget {
-  final SharedPreferences prefs;
 
   const MyApp({
     super.key,
-    required this.prefs,
   });
 
   @override
@@ -61,59 +38,44 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider<SocialAuthProvider>(
           create: (context) => SocialAuthProvider(
-            firebaseAuth: FirebaseAuth.instance,
-            googleSignIn: GoogleSignIn()
+            firebaseAuth: locator.get<FirebaseAuth>(),
+            googleSignIn: locator.get<GoogleSignIn>(instanceName: AppConstants.googleNormalSignInScope),
           ),
         ),
         ChangeNotifierProvider<ThemeProvider>(
           create: (context) => ThemeProvider(
-            prefs: prefs
+            prefs: locator.get<SharedPreferences>()
           ),
         ),
         Provider<SQFliteHelper>(
           create: (context) => SQFliteHelper(
-            prefs: prefs
+            prefs: locator.get<SharedPreferences>()
           ),
         ),
         ChangeNotifierProvider<GDriveProvider>(
           create: (context) => GDriveProvider(
-            localDB: context.read<SQFliteHelper>(),
-            googleSignIn:  GoogleSignIn(
-              scopes: [
-                'email',
-                'https://www.googleapis.com/auth/drive',
-              ]
-            )
+            localDB: locator.get<SQFliteHelper>(),
+            googleSignIn: locator.get<GoogleSignIn>(instanceName: AppConstants.googleDriveSignInScope)
           ),
         ),
         ChangeNotifierProvider<KeyProvider>(
           create: (context) => KeyProvider(
-            keyRepository: KeyRepository(
-                secureStorage: const FlutterSecureStorage()
-            ),
+            keyRepository: locator.get<KeyRepository>()
           ),
         ),
         ChangeNotifierProvider<CharactersProvider>(
           create: (context) => CharactersProvider(
-              charactersRepository: CharactersRepository(
-                  sqfliteHelper: context.read<SQFliteHelper>()
-              )
+              charactersRepository: locator.get<CharactersRepository>()
           ),
         ),
         ChangeNotifierProvider<ChatRoomsProvider>(
           create: (context) => ChatRoomsProvider(
-            chatRoomsRepository: ChatRoomRepository(
-                prefs: prefs,
-                sqfliteHelper: context.read<SQFliteHelper>()
-            ),
+            chatRoomsRepository: locator.get<ChatRoomRepository>()
           ),
         ),
         ChangeNotifierProvider<ChatProvider>(
           create: (context) => ChatProvider(
-            chatRepository: ChatRepository(
-              sqfliteHelper: context.read<SQFliteHelper>(),
-              prefs: prefs,
-            ),
+            chatRepository: locator.get<ChatRepository>(),
             charactersProvider: context.read<CharactersProvider>(),
             chatRoomsProvider: context.read<ChatRoomsProvider>(),
             keyProvider: context.read<KeyProvider>(),
