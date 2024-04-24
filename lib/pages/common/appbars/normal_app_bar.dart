97040f6b627d10f02b94d976a206913189333a52
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-24 22:37:07 +0900

    add common appbar

---
+++ lib/pages/common/appbars/normal_app_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class NormalAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool enableBackButton;
+
+  const NormalAppBar({
+    super.key,
+    required this.title,
+    this.enableBackButton = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        title,
+        style: TextStyle(
+          color: Theme.of(context).textTheme.bodyLarge!.color,
+        ),
+      ),
+      backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+      centerTitle: false,
+      leading: enableBackButton ? IconButton(
+        icon: const Icon(Icons.arrow_back_ios_new),
+        color: Colors.white,
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ) : null,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
