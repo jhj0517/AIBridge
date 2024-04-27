5b2d3f1e3448b1f70f3b28897ce884689aa80341
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-27 22:57:50 +0900

    remove function

--- lib/views/chat/widgets/messages/character/character_message_loading.dart
+++ lib/views/chat/widgets/messages/character/character_message_loading.dart
@@ -38,7 +38,29 @@ class CharacterMessageLoading extends BaseMessage{
                     Row(
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        _buildChatBox(),
+                        Flexible(
+                          child: Material(
+                            color: Colors.transparent,
+                            child: Ink(
+                                decoration: BoxDecoration(
+                                  color: ColorConstants.defaultCharacterChatBoxColor,
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(horizontal: 7.0, vertical: 8.0),
+                                  child: Container(
+                                    padding: const EdgeInsets.symmetric(horizontal: 7.0, vertical: 8.0),
+                                    child: Image.asset(
+                                      PathConstants.typingDotAnimation,
+                                      width: 30,
+                                      height: 20,
+                                      fit: BoxFit.fitHeight,
+                                    ), // replace with the path to your GIF
+                                  ),
+                                )
+                            ),
+                          ),
+                        ),
                         const SizedBox(width: 4.0),
                         buildTimestamp(context),
                         const SizedBox(width: 35.0),
@@ -51,32 +73,6 @@ class CharacterMessageLoading extends BaseMessage{
         ),
         const SizedBox(height: 8)
       ],
-    );
-  }
-
-  Widget _buildChatBox(){
-    return Flexible(
-      child: Material(
-        color: Colors.transparent,
-        child: Ink(
-            decoration: BoxDecoration(
-              color: ColorConstants.defaultCharacterChatBoxColor,
-              borderRadius: BorderRadius.circular(12.0),
-            ),
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 7.0, vertical: 8.0),
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 7.0, vertical: 8.0),
-                child: Image.asset(
-                  PathConstants.typingDotAnimation,
-                  width: 30,
-                  height: 20,
-                  fit: BoxFit.fitHeight,
-                ), // replace with the path to your GIF
-              ),
-            )
-        ),
-      ),
     );
   }
 
