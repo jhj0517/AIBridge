59e6a935fc9a683493fdc9100210497b3f28a9f0
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-27 22:10:28 +0900

    modularize into classes

---
+++ lib/views/chat/widgets/chat_list/chat_item.dart
@@ -0,0 +1,82 @@
+import 'package:aibridge/views/chat/widgets/messages/character/character_message.dart';
+import 'package:aibridge/views/chat/widgets/messages/character/character_message_delete_mdoe.dart';
+import 'package:aibridge/views/chat/widgets/messages/user/user_message.dart';
+import 'package:aibridge/views/chat/widgets/messages/user/user_message_delete_mode.dart';
+import 'package:flutter/material.dart';
+
+import 'package:aibridge/models/models.dart';
+import 'package:aibridge/views/chat/chat_page.dart';
+
+class ChatItem extends StatelessWidget {
+
+  const ChatItem({
+    super.key,
+    required this.item,
+    required this.chatTextEditingController,
+    required this.editChatFocusNode,
+    required this.messagesToDeleteNotifier,
+    required this.dialogCallback,
+    required this.settings,
+    this.character,
+    this.mode = ChatPageMode.chatMode
+  });
+
+  final ChatMessage item;
+  final TextEditingController chatTextEditingController;
+  final FocusNode editChatFocusNode;
+  final ValueNotifier<List<ChatMessage>> messagesToDeleteNotifier;
+  final Future<void> Function(ChatMessage) dialogCallback;
+  final ChatRoomSetting settings;
+  final ChatPageMode mode;
+  final Character? character;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (item.chatMessageType) {
+      case ChatMessageType.characterMessage:
+        if(mode==ChatPageMode.deleteMode){
+          return CharacterMessageDeleteMode(
+            character: character,
+            chatMessage: item,
+            settings: settings,
+            mode: mode,
+            messagesToDeleteNotifier: messagesToDeleteNotifier,
+            chatTextEditingController: chatTextEditingController,
+            editChatFocusNode: editChatFocusNode,
+          );
+        }
+        return CharacterMessage(
+          character: character,
+          chatMessage: item,
+          settings: settings,
+          mode: mode,
+          chatTextEditingController: chatTextEditingController,
+          editChatFocusNode: editChatFocusNode,
+          dialogCallback: dialogCallback,
+        );
+
+      case ChatMessageType.userMessage:
+        if(mode==ChatPageMode.deleteMode){
+          return UserMessageDeleteMode(
+            chatMessage: item,
+            settings: settings,
+            mode: mode,
+            messagesToDeleteNotifier: messagesToDeleteNotifier,
+            chatTextEditingController: chatTextEditingController,
+            editChatFocusNode: editChatFocusNode,
+          );
+        }
+        return UserMessage(
+          chatMessage: item,
+          settings: settings,
+          mode: mode,
+          chatTextEditingController: chatTextEditingController,
+          editChatFocusNode: editChatFocusNode,
+          dialogCallback: dialogCallback,
+        );
+      default:
+    }
+    return const SizedBox.shrink();
+  }
+
+}
---
+++ lib/views/chat/widgets/chat_list/chat_list.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:aibridge/providers/chat_provider.dart';
+import 'package:aibridge/models/models.dart';
+import 'package:aibridge/views/chat/chat_page.dart';
+import 'chat_item.dart';
+
+class ChatList extends StatefulWidget{
+
+  const ChatList({
+    super.key,
+    required this.list,
+    required this.character,
+    required this.isLoading,
+    required this.mode,
+    required this.chatScrollController,
+    required this.chatTextEditingController,
+    required this.editChatFocusNode,
+    required this.messagesToDeleteNotifier,
+    required this.dialogCallback,
+    required this.settings,
+  });
+
+  final List<ChatMessage> list;
+  final Character character;
+  final bool isLoading;
+  final ChatPageMode mode;
+  final ScrollController chatScrollController;
+  final TextEditingController chatTextEditingController;
+  final FocusNode editChatFocusNode;
+  final ValueNotifier<List<ChatMessage>> messagesToDeleteNotifier;
+  final Future<void> Function(ChatMessage) dialogCallback;
+  final ChatRoomSetting settings;
+
+  @override
+  ChatListState createState() => ChatListState();
+}
+
+class ChatListState extends State<ChatList> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ChatProvider>(
+      builder: (context, chatProvider, _) { // reverse solution: //solution from https://stackoverflow.com/questions/70577942/flutter-resizetoavoidbottominset-true-not-working-with-expanded-listview
+        final chatMessages = chatProvider.chatMessages.reversed.toList();
+        return Container(
+          color: widget.settings.chatRoomBackgroundColor,
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: ListView.builder(
+                shrinkWrap: true,
+                reverse: true,
+                cacheExtent: widget.isLoading? double.maxFinite: null, // without this, scrollChatToBottom() at first does not work
+                padding: const EdgeInsets.only(top: 10),
+                controller: widget.chatScrollController,
+                itemCount: chatMessages.length,
+                itemBuilder: (context, index) {
+                  final item = chatMessages[index];
+                  return ChatItem(
+                    item: item,
+                    character: widget.character,
+                    chatTextEditingController: widget.chatTextEditingController,
+                    editChatFocusNode: widget.editChatFocusNode,
+                    messagesToDeleteNotifier: widget.messagesToDeleteNotifier,
+                    dialogCallback: widget.dialogCallback,
+                    settings: widget.settings,
+                  );
+                }
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+}
