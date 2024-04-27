4880eabfaf066e827be149c7cd2706d3b6707727
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-27 13:24:06 +0900

    modularize to classes

--- lib/views/chat/chat_page.dart
+++ lib/views/chat/chat_page.dart
@@ -17,6 +17,7 @@ import '../views.dart';
 import 'package:aibridge/views/chat/widgets/messages/user/user_message.dart';
 import 'package:aibridge/views/chat/widgets/messages/user/user_message_delete_mode.dart';
 import 'package:aibridge/views/chat/widgets/messages/character/character_message.dart';
+import 'package:aibridge/views/chat/widgets/messages/character/character_message_delete_mdoe.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key, required this.arguments}) : super(key: key);
