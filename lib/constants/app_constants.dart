00bc97b6181164262fe12bc7cacdec8bc49d021c
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-30 00:38:02 +0900

    add usage policy link

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,6 +1,8 @@
 class AppConstants {
   static const privacyPolicyEN = "https://docs.google.com/document/d/1jqJidv7-r0o_1E5JJrP3ebDIbMw05jM_Wl300hIWBlk/edit?usp=sharing";
   static const privacyPolicyKO = "https://docs.google.com/document/d/1gIpFSEZ6hHXe7lBepQXcO7hp0X7pZNRorcLO8Y3l1Ow/edit?usp=sharing";
+  static const usagePolicyEN = "https://docs.google.com/document/d/1ogV9Guimmsb4ar5ByGlmz3IQvfKxxKBqgWV7TuURQFM/edit?usp=sharing";
+  static const usagePolicyKO = "https://docs.google.com/document/d/1-jLS2wo6UIebObnwt_mJYGgjxtpJf511w_zudyGkVGY/edit?usp=sharing";
   static const openAIUsagePolicy = "https://openai.com/policies/usage-policies";
   static const googleDriveSignInScope = "Google Drive Sign In";
   static const googleNormalSignInScope = "Google Normal Sign In";
